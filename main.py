b62fe17146a6c89670e2c2c1238652c266c819a9
Claudiu357 <neculaclaudiu21@gmail.com>
2025-02-12 21:26:57 +0100

    i implemeted the algorithm for the reset button and i finished te project

--- main.py
+++ main.py
@@ -6,12 +6,20 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 1
+WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
+timer = None
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+# ---------------------------- TIMER RESET ------------------------------- #
+def reset_timer():
+     window.after_cancel(timer)
+     timer_label.config(text= "Timer", fg = GREEN)
+     canvas.itemconfig(timer_text,text = "00:00")
+     check_marks.config(text="")
+     global reps
+     reps = 0
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
@@ -38,9 +46,17 @@ def count_down(count):
         count_seconds = f"0{count_seconds}"
     canvas.itemconfig(timer_text, text = f"{count_min}:{count_seconds}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
     else:
         start_timer()
+        mark = ""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            mark += "✔"
+        check_marks.config(text=mark)
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Tomato Timer")
@@ -49,13 +65,13 @@ window.config(padx=100, pady= 50, bg=YELLOW)
 start_button = Button(text="Start", command=start_timer)
 start_button.grid(row = 2, column = 0)
 
-reset_button = Button(text = "Reset")
+reset_button = Button(text = "Reset", command=reset_timer)
 reset_button.grid(row = 2, column = 2)
 
 timer_label = Label(text="Timer", bg=YELLOW, fg=GREEN ,font=(FONT_NAME, 35, "bold"))
 timer_label.grid(row = 0, column = 1)
 
-check_marks = Label(text="✔", bg=YELLOW,fg=GREEN, font=(FONT_NAME, 13))
+check_marks = Label(bg=YELLOW,fg=GREEN, font=(FONT_NAME, 13))
 check_marks.grid(row = 3, column = 1)
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
