f1f0f9841e6a84803f8bfa7094065f7265bf1edf
Claudiu357 <neculaclaudiu21@gmail.com>
2025-02-12 19:06:51 +0100

    i added the time mechanism algorithm

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -11,16 +12,24 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def start_timer():
+    count_down(300)
 
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_seconds = count % 60
+    canvas.itemconfig(timer_text, text = f"{count_min}:{count_seconds}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Tomato Timer")
 window.config(padx=100, pady= 50, bg=YELLOW)
 
-start_button = Button(text="Start")
+start_button = Button(text="Start", command=start_timer)
 start_button.grid(row = 2, column = 0)
 
 reset_button = Button(text = "Reset")
@@ -35,7 +44,7 @@ check_marks.grid(row = 3, column = 1)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image = tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(row = 1, column = 1)
 
 
